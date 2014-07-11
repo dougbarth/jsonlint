30bb382a68ac4d7ee4f6ee226d968e9302821c6f
Doug Barth <dougbarth@gmail.com>
2014-07-11 15:04:00 -0700

    Adding Aruba
    The aruba gem makes it possible to test a CLI app. I hate Cucumber, so
    I'm not using that approach. This blog post helps:
    
    http://fractio.nl/2013/12/06/cli-testing-with-rspec-and-cucumber-less-aruba/
    
    Fixes #4

--- lib/jsonlint/cli.rb
+++ lib/jsonlint/cli.rb
@@ -1,3 +1,5 @@
+require 'trollop'
+
 module JsonLint
   class CLI
     def initialize(argv, stdin = STDIN, stdout = STDOUT, stderr = STDERR, kernel = Kernel)
@@ -5,7 +7,11 @@ module JsonLint
     end
 
     def execute!
+      parse_options
+
       files_to_check = @argv
+
+      Trollop::die 'nead at least one JSON file to check' if files_to_check.empty?
 
       linter = JsonLint::Linter.new
       linter.check_all(files_to_check)
@@ -14,6 +20,22 @@ module JsonLint
         linter.display_errors
         @kernel.exit(1)
       end
+    end
+
+    private
+
+    def parse_options
+      @opts = Trollop::options(@argv) do
+        banner 'Usage: jsonlint [options] file1.json [file2.json ...]'
+        version(JsonLint::VERSION)
+
+        banner ''
+        banner 'Options:'
+      end
+    end
+
+    def opts
+      @opts
     end
   end
 end
---
+++ spec/cli_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'jsonlint' do
+  it 'should print usage if run with no args' do
+    jsonlint
+    assert_failing_with('Error')
+  end
+
+  it '-h should print usage' do
+    jsonlint '-h'
+    assert_passing_with('Usage')
+  end
+
+  it '--help should print usage' do
+    jsonlint '--help'
+    assert_passing_with('Usage')
+  end
+
+  it '-v should print its version' do
+    jsonlint '-v'
+    assert_passing_with(JsonLint::VERSION)
+  end
+
+  it '--version should print its version' do
+    jsonlint '--version'
+    assert_passing_with(JsonLint::VERSION)
+  end
+end
